bc0d0c0d5bfb6f05c2ff3311439c6e906d759448
Josh Click <joshuaqian8@gmail.com>
2014-03-31 13:21:17 -0400

    Update based on PR comment - #9 - https://www.pivotaltracker.com/story/show/67918322 [Fixes #67918322]

--- db/migrate/20140326191654_add_version_to_tasks.rb
+++ db/migrate/20140326191654_add_version_to_tasks.rb
@@ -1,5 +1,9 @@
 class AddVersionToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :version, :integer, default: 1
+    Task.reset_column_information
+    reversible do |dir|
+      dir.up { Task.update_all version: 1 }
+    end
   end
 end
